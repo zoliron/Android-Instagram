99d5b20d3de26b9d9a7e00c261d9c18c544de9e7
Ronen Zolicha <ronen1245@gmail.com>
2019-07-16 13:09:28 +0300

    Login and Register layout implementation

---
+++ app/src/main/java/zoli/instagram/RegisterActivity.java
@@ -0,0 +1,27 @@
+package zoli.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, fullname, email, password;
+    Button register;
+    TextView txt_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        txt_login = findViewById(R.id.txt_login);
+    }
+}
