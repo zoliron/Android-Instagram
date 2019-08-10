004af8a67c9fd14b4d4b90156cef0980017b4c28
Ronen Zolicha <ronen1245@gmail.com>
2019-08-10 13:06:51 +0300

    Update likes to pop up notification

--- app/src/main/java/zoli/instagram/Adapter/PostAdapter.java
+++ app/src/main/java/zoli/instagram/Adapter/PostAdapter.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.List;
 
 import zoli.instagram.CommentsActivity;
@@ -138,6 +139,7 @@ public class PostAdapter  extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
                 if (holder.like.getTag().equals("like")){
                     FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid())
                             .child(firebaseUser.getUid()).setValue(true);
+                    addNotifications(post.getPublisher(), post.getPostid());
                 } else {
                     FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid())
                             .child(firebaseUser.getUid()).removeValue();
@@ -241,6 +243,19 @@ public class PostAdapter  extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
             }
         });
     }
+
+    private void addNotifications(String userid, String postid) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Notifications").child(userid);
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("userid", firebaseUser.getUid());
+        hashMap.put("text", "Liked your post");
+        hashMap.put("postid", postid);
+        hashMap.put("ispost", true);
+
+        reference.push().setValue(hashMap);
+    }
+
 
     //Get numbers of likes
     private void nrLikes(final TextView likes, String postid){
